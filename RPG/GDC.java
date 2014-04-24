bff39694dbfc12cfc6ec673b005c277d27ab4721
jcboysha <jcboysha.117@gmail.com>
2014-04-23 20:34:41 -0400

    Code update
    Changed the test code in GDC to test the room output of contents.

--- RPG/GDC.java
+++ RPG/GDC.java
@@ -10,8 +10,10 @@ public class GDC
 {
    public static void main(String args[])
    {
-       System.out.println("Room Debug");
+       
        Room room = new Room();
+       Player player = new Player();
+       /*System.out.println("Room Debug");
        room.openNorth();
        room.enterRoom();
        room.debugRoom();
@@ -22,12 +24,30 @@ public class GDC
        room.debugRoom();
        
        System.out.println("\nPlayer Debug");
-       Player player = new Player();
        player.setName("Daelphinux");
        player.debugPlayer();
        System.out.println("\nPlayer Change");
        player.setHealth(-3);
        player.setPower(1);
        player.debugPlayer();
+       */
+       System.out.println("\nRoom Content Test:");
+       for(int i = 0; i <= 5; i++){
+           room.enterRoom();
+           int c1 = room.getContents();
+           System.out.print(c1 + ": ");
+           if (c1<700){
+               System.out.println("A MONSTER!");
+           }
+           else if (c1>700 && c1<=850){
+               System.out.println("NOTHING HAPPENS!");
+           }
+           else if(c1>850 && c1<=998){
+               System.out.println("AN ITEM!");
+           }
+           else{
+               System.out.println("AN EXIT!");
+           }
+        }
    }
 }
